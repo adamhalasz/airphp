a4fb1850dac48426700c990fee0338547badeb08
Adam Halasz <mail@adamhalasz.com>
2018-05-14 13:51:42 +0200

    Create base class

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<?php 
+class Air {
+    public $routes;
+    public $data;
+    public $components;
+    public $scripts;
+    public $styles;
+
+    function __construct() {
+        $routes = (object) [];
+        $data = (object) [];
+    }
+
+    // add route
+    function route($options){
+
+    }
+    // add component
+    function component($component_name){
+
+    }
+
+    // create global script
+    function script($component_name){
+
+    }
+
+    // create global stylesheet
+    function style($component_name){
+
+    }
+
+    function start(){
+
+    }
+}
+?>
